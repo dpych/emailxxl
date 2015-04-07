8c19d73d0de0dffc8987857eb77fb602fcb202a2
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-07 16:00:47 +0200

    views

--- src/view/products.php
+++ src/view/products.php
@@ -1,18 +1,46 @@
-<?php 
-    $i=0;
-    $col=0;
-?>
-<table width="600px" cellspacing="0" cellspadding="0">
+<?php foreach ($elements as $typ => $groups) : ?>
+<table width="570px" border="0" cellpadding="0" cellspacing="0" style="width:570px; margin: 0 15px">
+    <thead>
+        <tr>
+            <th colspan="3" style="margin: 0 -15px;">
+                <?php if($typ === 0) : ?>
+                <img src="images/mailing_03.jpg" alt="Wszędzie" />
+                <?php endif; ?>
+                <?php if($typ === 1) : ?>
+                <img src="images/mailing_07.jpg" alt="Tylko wirtualny koszyk" />
+                <?php endif; ?>
+            </th>
+        </tr>
+    </thead>
     <tbody>
+        <?php foreach($groups as $key => $products ) : ?>
+        <?php 
+            $i=0;
+            $col=0;
+        ?>
+        <?php if(count($products) > 0 ) : ?>
+        <tr>
+            <td colspan="3"  style="margin: 0 -15px;">
+                <?php if($key=='meskie') : ?>
+                <img src="images/mailing_04.jpg" alt="Męskie" />
+                <?php endif; ?>
+                <?php if($key=='damskie') : ?>
+                <img src="images/mailing_05.jpg" alt="Damskie" />
+                <?php endif; ?>
+                <?php if($key=='dzieciece') : ?>
+                <img src="images/mailing_06.jpg" alt="Dziecięce" />
+                <?php endif; ?>
+            </td>
+        </tr>
         <tr>
             <?php 
             foreach($products as $item) : 
                 $i++;
                 $col++;
             ?>
-            <td width="33%" style="font-family: Verdana, Geneva, sans-serif; font-size: 11px; vertical-align: top; text-transform: uppercase;">
-                <a href="<?php echo $item->url ?>" target="_blank" title="Product" style="color: #000; text-decoration: none;">
-                    <img src="<?php echo $item->image ?>" alt="Produkt" width="100%" />
+            <td width="33%" style="font-family: Verdana, Geneva, sans-serif; font-size: 11px; vertical-align: top; text-transform: uppercase; padding: 5px;">
+                <a href="<?php echo $item->url ?>utm_content=produkt&amp;utm_campaign=M_<?php echo date('d_m_Y'); ?>_Wirtualny_Koszyk_U&amp;smclient=$$id_salesmanago$$" target="_blank" title="<?php echo $item->attributes->attribute[0]->value . $item->name ?>" style="color: #000; text-decoration: none;">
+                    <img src="<?php echo $item->image ?>" alt="<?php echo $item->attributes->attribute[0]->value . $item->name ?>" width="100%" />
                     <span style="font-weight:bold"><?php echo $item->attributes->attribute[0]->value; ?></span><br />
                     <?php echo $item->name; ?>
                 </a>
@@ -25,6 +53,12 @@
         <tr>
             <?php endif; ?>
             <?php endforeach; ?>
+            <?php if($col) : ?>
+            <td width="<?php echo (3-$col)*33 ?>%" colspan="<?php echo 3-$col ?>"></td>
+            <?php endif; ?>
         </tr>
+        <?php endif; ?>
+        <?php endforeach; ?>
     </tbody>
 </table>
+<?php endforeach; ?>
