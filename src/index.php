447bb5d4fa610f92d877884156ee3cf868dbc25a
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-07 16:35:00 +0200

    load role for index, remove classes to app and framework directories

--- src/index.php
+++ src/index.php
@@ -1,112 +1,54 @@
-<?php 
-abstract class Model {
-    protected $source;
-    protected $_data;
-    
-    abstract public function getData();
-}
+<?php
+define('DS', '/');
+define('APP', './app');
+define('CORE', './framework');
+define('VIEW', '/view');
 
-abstract class Model_XML extends Model {
-    protected $_data = array();
-    protected $_reader = NULL;
-    protected $_offset = 'offer';
-    
-    public function __construct() {
-        $this->loadData();
+$extensions = array('.php');
+$ext = implode(',',$extensions);
+spl_autoload_register(null, false);
+spl_autoload_extensions($ext);
+ 
+function frameworkLoader($classname){
+    $extensions = array('.php');
+    if (class_exists($classname, false)){ return; }
+    $class = explode('_', strtolower(strval($classname)));
+    $deeps = count($class);
+    $file = CORE;
+    for ($i=0;$i<$deeps;$i++){
+        $file .= DS . $class[$i];
     }
-    
-    public function getData() {
-        return $this->_data;
-    }
-
-    public function getDataDetails($id) {
-        if($id) {
-            return isset($this->_data[$id]) ? $this->_data[$id] : NULL ;
+    foreach ($extensions as $ext){
+        $fileClass = $file.$ext;
+        if (file_exists($fileClass) && is_readable($fileClass) && !class_exists($classname, false)){
+            require_once($fileClass);
+            return true;
         }
     }
-    
-    private function loadData() {
-        $this->_reader = new XMLReader();
-        $this->_reader->open($this->source);
-        
-        while($this->_reader->read()) {
-            if($this->_reader->nodeType == XMLReader::ELEMENT && $this->_reader->name == $this->_offset)
-            {
-                $doc = new DOMDocument('1.0', 'UTF-8');
-                $element = simplexml_import_dom($doc->importNode($this->_reader->expand(),true));
-                $this->_data[trim($element->id)] = $element;
-            }
+    return false;
+}
+
+function appLoader($classname){
+    $extensions = array('.php');
+    if (class_exists($classname, false)){ return; }
+    $class = explode('_', strtolower(strval($classname)));
+    $deeps = count($class);
+    $file = APP;
+    for ($i=0;$i<$deeps;$i++){
+        $file .= DS . $class[$i];
+    }
+    foreach ($extensions as $ext){
+        $fileClass = $file.$ext;
+        if (file_exists($fileClass) && is_readable($fileClass) && !class_exists($classname, false)){
+            require_once($fileClass);
+            return true;
         }
     }
+    return false;
 }
 
-abstract class Model_CSV extends Model {
-
-    protected $_data = array();
-    protected $_separator = ";";
-
-    public function __construct() {
-        $this->loadData();
-    }
-
-    public function getData() {
-        return $this->_data;
-    }
-    
-    private function loadData() {
-        $file = fopen($this->source, "r");
-        while( ($data = fgetcsv($file, 0, $this->_separator)) !== FALSE ) {
-             $this->_data[] = $data;
-        }
-    }
-}
-
-class Model_SizeerCom extends Model_XML {
-    protected $source = 'http://sklep.sizeer.com/comparators/NEW_CENEO.xml';    
-}
-
-class Model_Products extends Model_CSV {
-    protected $source = 'products.csv';
-}
-
-
-class View {
-    public static function factory($path, $params = array()) {
-        foreach($params as $key => $value) {
-            $$key = $value;
-        }
-        ob_start();
-        include_once $path;
-        $html = ob_get_contents();
-        ob_end_clean();
-        return $html;
-    }
-}
-
-class ProductsEmail {
-    
-    private $products;
-    
-    public function __construct() {
-        $this->generateProductView();
-        $this->generateEmail();
-    }
-    
-    private function generateProductView() {
-        $xml = new Model_SizeerCom();
-        $csv = new Model_Products();
-
-        $products = array();
-        foreach($csv->getData() as $item ) {
-            $products[] = $xml->getDataDetails((int) $item[0]);
-        }
-        $this->products = View::factory('./view/products.php', array('products' => $products));
-    }
-    
-    private function generateEmail() {
-        echo View::factory('./view/template.php', array('products' => $this->products));
-    }
-}
+spl_autoload_register('frameworkLoader');
+spl_autoload_register('appLoader');
 
 function main() {
     new ProductsEmail();
