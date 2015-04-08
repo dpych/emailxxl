c8b126d3df6120882e2a9cc5481a832138f5017a
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-08 16:28:18 +0200

    new action in controller

--- src/app/controller/main.php
+++ src/app/controller/main.php
@@ -1,13 +1,23 @@
 <?php
 class Controller_Main extends Controller {
-    public function index($get) {
+    
+    public function index() {
+        echo View::factory('index.php');
+    }
+    
+    public function download() {
         $email = new ProductsEmail();
         $email->run();
         $files = array();
         $files[$email->getFileName()] = date('Ymd') . '.html';
 
         Zip::archive($files, './download/' . date('Ymd'));
+        Files::delete('./download/' . date('Ymd') . '.html');
         
         $this->redirect('./download/' . date('Ymd') . '.zip');
+    }
+    
+    public function upload() {
+        
     }
 }
