1866ec84cbac785a48c2296836a0e64562791049
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-08 16:11:04 +0200

    index view

---
+++ src/app/view/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="?c=main&a=upload" methon="POST" enctype="multipart/form-data">
+            <div>
+                <label>Plik z produktami</label>
+                <input type="file" name="csv" />
+            </div>
+            <div>
+                <button type="submit">Generuj</button>
+            </div>
+        </form>
+    </body>
+</html>
