c6492617e5005b08a1dc9f8ed127c55986b35fc7
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-14 10:07:52 +0200

    override default method

--- src/app/model/sizeercom.php
+++ src/app/model/sizeercom.php
@@ -8,4 +8,17 @@ class Model_SizeerCom extends Model_XML {
         }
     }
     
+    private function loadData() {
+        $this->_reader = new XMLReader();
+        $this->_reader->open($this->source);
+        
+        while($this->_reader->read()) {
+            if($this->_reader->nodeType == XMLReader::ELEMENT && $this->_reader->name == $this->_offset)
+            {
+                $doc = new DOMDocument('1.0', 'UTF-8');
+                $element = simplexml_import_dom($doc->importNode($this->_reader->expand(),true));
+                $this->_data[trim($element->attributes->attribute[5]->value)] = $element;
+            }
+        }
+    }
 }
