1b4cdfd78b73fff3d28fe5ae97cc19585a202797
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-04-14 10:06:20 +0200

    add getDataDetail

--- src/app/model/sizeercom.php
+++ src/app/model/sizeercom.php
@@ -1,4 +1,11 @@
 <?php
 class Model_SizeerCom extends Model_XML {
     protected $source = 'http://sklep.sizeer.com/comparators/NEW_CENEO.xml';    
+    
+    public function getDataDetails($id) {
+        if($id) {
+            return isset($this->_data[$id]) ? $this->_data[$id] : NULL ;
+        }
+    }
+    
 }
