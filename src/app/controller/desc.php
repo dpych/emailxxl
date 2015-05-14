dca13749cf87eb06fb86834c66f780af1eb75a5c
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-14 15:11:53 +0200

    error messages

--- src/app/controller/desc.php
+++ src/app/controller/desc.php
@@ -10,60 +10,79 @@ class Controller_Desc extends Controller {
     }
     
     public function upload() {
-        if($_FILES['products']) {
-            $excel = PHPExcel_IOFactory::load($_FILES['products']['tmp_name']);
-            $tmp   = new PHPExcel();
+        if($_FILES['products']["name"]) {
             
-            if(!is_dir('./download'))
-                mkdir('./download', 0777, TRUE);
-            
-            $file = fopen('./download/products_desc.xml','w');
-            $dane = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-                    <!DOCTYPE pasaz:Envelope SYSTEM \"loadOffers.dtd\">
-                    <pasaz:envelope xmlns:pasaz=\"http://schemas.xmlsoap.org/soap/envelope/\">
-                    <pasaz:Body><loadOffers xmlns=\"urn:ExportB2B\"><offers>
-                    ";
-            fwrite($file, $dane);
-            
-            foreach( $excel->getWorksheetIterator() as $key => $worksheet ) {
-                
-                if($key == 0) {
-                    echo $key . '<br />';
-                    $tmp_worksheep = $tmp->setActiveSheetIndex(0);
-                    $rows = $worksheet->getHighestRow();
-                    $columns = PHPExcel_Cell::columnIndexFromString($worksheet->getHighestColumn());
-                    for($row=1; $row <= $rows; ++$row) {
-                        for($col=0; $col<$columns; ++$col) {
-                            $cell = $worksheet->getCellByColumnAndRow($col,$row);
-                            $val = $cell->getValue();
-
-                            $val = str_replace(array("\n\r","\r","\n"), "", $val);
-
-                            $tmp_worksheep->setCellValueByColumnAndRow($col, $row, $val);
-                            $tmp_cell = $worksheet->getCellByColumnAndRow($col,$row);
-                            $val = $tmp_cell->getValue();
-
-                            switch ($col) {
-                                case 0:
-                                    $id = $val;
-                                    break;
-                                case 1:
-                                    $name = $val;
-                                    break;
-                                case 2:
-                                    $description = $val;
-                                    break;
-                            }   
-                        }
-                        $this->printproducts($id, $name, $description, $file);
-                    }
-                }    
+            if(!isset($_FILES['products'])) {
+                $_FILES = array(
+                    'products' => array('tmp_name'=>false)
+                );
+                $fileType = array(1=>false);
+            } else {
+                $fileType = explode('.', $_FILES['products']['name']);
             }
 
-            fwrite($file, "</offers></loadOffers></pasaz:Body></pasaz:envelope>");
-            fclose($file);
+            if($fileType[1] == 'xlsx') {
+            
+                $excel = PHPExcel_IOFactory::load($_FILES['products']['tmp_name']);
+                $tmp   = new PHPExcel();
+
+                if(!is_dir('./download'))
+                    mkdir('./download', 0777, TRUE);
+
+                $file = fopen('./download/products_desc.xml','w');
+                $dane = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        ."<!DOCTYPE pasaz:Envelope SYSTEM \"loadOffers.dtd\">\n"
+                        ."    <pasaz:envelope xmlns:pasaz=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                        ."    <pasaz:Body>\n"
+                        ."        <loadOffers xmlns=\"urn:ExportB2B\">\n"
+                        ."            <offers>\n";
+                fwrite($file, $dane);
+
+                foreach( $excel->getWorksheetIterator() as $key => $worksheet ) {
+
+                    if($key == 0) {
+                        echo $key . '<br />';
+                        $tmp_worksheep = $tmp->setActiveSheetIndex(0);
+                        $rows = $worksheet->getHighestRow();
+                        $columns = PHPExcel_Cell::columnIndexFromString($worksheet->getHighestColumn());
+                        for($row=1; $row <= $rows; ++$row) {
+                            for($col=0; $col<$columns; ++$col) {
+                                $cell = $worksheet->getCellByColumnAndRow($col,$row);
+                                $val = $cell->getValue();
+
+                                $val = str_replace(array("\n\r","\r","\n"), "", $val);
+
+                                $tmp_worksheep->setCellValueByColumnAndRow($col, $row, $val);
+                                $tmp_cell = $worksheet->getCellByColumnAndRow($col,$row);
+                                $val = $tmp_cell->getValue();
+
+                                switch ($col) {
+                                    case 0:
+                                        $id = $val;
+                                        break;
+                                    case 1:
+                                        $name = $val;
+                                        break;
+                                    case 2:
+                                        $description = $val;
+                                        break;
+                                }   
+                            }
+                            $this->printproducts($id, $name, $description, $file);
+                        }
+                    }    
+                }
+
+                fwrite($file, "</offers></loadOffers></pasaz:Body></pasaz:envelope>");
+                fclose($file);
+                $this->setMsg('Plik wysłany. Można importować w sklepie', 'success');
+            } else {
+                $this->setMsg('Zły typ pliku', 'danger');
+            }
+        } else {
+            $this->setMsg('Nie załączono pliku <strong>Exel</strong>', 'danger');
         }
-        $this->setMsg('Plik pobrany', 'info');
+        
         $this->redirect('?c=desc');
     }
     
