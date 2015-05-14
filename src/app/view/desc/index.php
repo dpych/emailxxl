02b7e003bf1a600650cc7b8136a446230ffe9126
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-14 11:54:52 +0200

    added bootstrap 3.3 styles

--- src/app/view/desc/index.php
+++ src/app/view/desc/index.php
@@ -1,25 +1,37 @@
 <html>
     <head>
-        
+        <link href="lib/css/bootstrap.css" type="text/css" rel="stylesheet" />
+        <style type="text/css">
+            body {padding-top: 60px;}
+        </style>
     </head>
     <body>
-        <div>
-            <div class="">
+        <header class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="index.php?c=desc">Import opisów</a>
+            </div>
+        </header>
+        <div class="container">
+            <?php if(isset($msg['msg'])) : ?>
+            <div class="alert alert-<?php echo $msg['type'] ? $msg['type'] : 'info'?>">
                 <?php echo isset($msg['msg'])? $msg['msg'] : "" ; ?>    
             </div>
-            <form action="?c=desc&a=upload" method="POST" enctype="multipart/form-data">
-                <div>
-                    <input type="file" name="products" />
-                </div>
-                <div>
-                    <select disabled="disabled">
-                        <option value="1">sklep.sizeer.com</option>
-                    </select>
-                </div>
-                <div>
-                    <button class="btn btn-primary">Wyślij</button>
-                </div>
-            </form>
+            <?php endif; ?>
+            <div class="row">
+                <form action="?c=desc&a=upload" method="POST" enctype="multipart/form-data" class="col-xs-5">
+                    <div class="form-group">
+                        <input type="file" name="products" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <select disabled="disabled" class="form-control">
+                            <option value="1">sklep.sizeer.com</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-primary">Wyślij</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
