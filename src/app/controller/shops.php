13e4802e0439aab1ca64a8ac92f6e13322c92926
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-20 10:27:54 +0200

    index method

--- src/app/controller/shops.php
+++ src/app/controller/shops.php
@@ -6,6 +6,9 @@
  */
 class Controller_Shops extends Controller {
     public function index() {
-        echo View::factory('shops/index.php');
+        $model_shops = new Model_Shops();
+        $shops = $model_shops->getData();
+        echo View::factory('shops/index.php', array('shops'=>$shops));
     }
+    
 }
