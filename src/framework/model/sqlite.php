78adf8c8bcbc0d1f9747842badb149540acc3fba
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-20 10:04:22 +0200

    create table method

--- src/framework/model/sqlite.php
+++ src/framework/model/sqlite.php
@@ -7,8 +7,15 @@ abstract class Model_Sqlite extends Model {
     
     public function __construct() {
         $this->_data = new SQLite3($this->source);
+        if ( (bool) $this->_schemat ) {
+            $this->createTable();
+        }
         return $this;
     }
 
     abstract public function getData() {}
+    
+    private function createTable() {
+        $this->_data->exec($this->_schemat);
+    }
 }
