e130a1b5460a9c48725973c5cafb6428b5161c5e
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-29 11:47:41 +0200

    redirect for user

--- src/app/model/auth.php
+++ src/app/model/auth.php
@@ -2,10 +2,18 @@
 class Model_Auth extends Model {
     
     protected $users = array(
-        'admin'=>'Adamek123'
+        'admin'=>'Adamek123',
+        'piotr'=> 'PiotrusPan'
     );
 
-
+    protected $main = array(
+        'piotr'=>'?c=main'
+    );
+    
+    public function getRedirects() {
+        return $this->main;
+    }
+    
     public function getData() {
         return $this->users;
     }
