ca7e08fed9c0359539e541de1c70b3366dbb08ab
dpych <dpych@PC-MBARCZYK.mig.lan>
2015-05-29 10:10:56 +0200

    change save upload file

--- src/app/view/index.php
+++ src/app/view/index.php
@@ -6,18 +6,35 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="lib/css/bootstrap.css" type="text/css" rel="stylesheet" />
+        <style type="text/css">
+            body {padding-top: 60px;}
+        </style>
     </head>
     <body>
-        <form action="?c=main&a=download" method="POST" enctype="multipart/form-data">
-            <div>
-                <label>Plik z produktami</label>
-                <input type="file" name="csv" />
+        <header class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <a href="index.php?c=main" class="navbar-brand">Wirtualna pułka</a>
             </div>
-            <div>
-                <button type="submit">Generuj</button>
+        </header>
+        <div class="container">
+            <div class="row">
+                <?php if(isset($msg['msg'])) : ?>
+                <div class="alert alert-<?php echo $msg['type'] ? $msg['type'] : 'info'?>">
+                    <?php echo isset($msg['msg'])? $msg['msg'] : "" ; ?>    
+                </div>
+                <?php endif; ?>
+                <form action="?c=main&a=upload" method="POST" enctype="multipart/form-data" class="form-inline col-xs-4">
+                    <div class="form-group-sm">
+                        <label>Plik z produktami</label>
+                        <input type="file" name="csv" class="form-control" />
+                        <button type="submit" class="btn btn-primary btn-sm">Generuj</button>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </body>
 </html>
